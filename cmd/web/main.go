7d8bc1d5e5d847611712f02d52c2da7507f8fad2
MjLee <arrogantmj@gmail.com>
2022-10-18 09:08:06 +0900

    Finished section_01

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/contractShield/gowebdev/pkg/config"
 	"github.com/contractShield/gowebdev/pkg/handlers"
+	"github.com/contractShield/gowebdev/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("connot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandler(repo)
+
+	render.NewTemplates(&app)
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
--- pkg/handlers/handers.go
+++ pkg/handlers/handers.go
@@ -3,15 +3,44 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/contractShield/gowebdev/pkg/config"
+	"github.com/contractShield/gowebdev/pkg/models"
 	"github.com/contractShield/gowebdev/pkg/render"
 )
 
+// Repo the repository used by Handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHanders sets the repository for the Handlers
+func NewHandler(r *Repository) {
+	Repo = r
+}
+
 // Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page hander
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "hello,again"
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
